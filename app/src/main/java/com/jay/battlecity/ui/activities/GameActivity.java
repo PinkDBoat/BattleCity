d46e886e978898bc035443c6a425ddd1891480c4
Jay <1432562823@qq.com>
2017-04-09 21:46:34 +0800

    完成部分主界面UI

---
+++ app/src/main/java/com/jay/battlecity/ui/activities/GameActivity.java
@@ -0,0 +1,14 @@
+package com.jay.battlecity.ui.activities;
+
+import android.os.Bundle;
+
+import com.jay.battlecity.R;
+
+public class GameActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
